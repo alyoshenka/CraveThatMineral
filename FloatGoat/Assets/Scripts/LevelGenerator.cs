4057433ade58b2a34297816e2dcd3e2f84471dc3
Alexi Most <alexibmost@gmail.com>
2019-05-15 15:45:18 -0700

    oops

--- FloatGoat/Assets/Scripts/LevelGenerator.cs
+++ FloatGoat/Assets/Scripts/LevelGenerator.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform player;
+    public int tunnelLength;
+    public GameObject wall;
+    public float resetVal;
+
+    GameObject[] walls;
+    int wallIdx;
+    float currentWallZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        walls = new GameObject[tunnelLength];
+        wallIdx = 0;
+        currentWallZ = walls[wallIdx].transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.z > currentWallZ + resetVal) { WrapWalls(); }
+    }
+
+    void WrapWalls()
+    {
+
+    }
+
+    GameObject WallPrev()
+    {
+        return wallIdx == 0 ? walls[tunnelLength - 1] : walls[wallIdx];
+    }
+
+
 }
